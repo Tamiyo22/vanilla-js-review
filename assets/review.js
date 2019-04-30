3289e789dc599a5764907e0d6455be503d91436a
Tamiyo22 <melissaeaybar@gmail.com>
2019-04-30 14:08:27 -0500

    base review of some event handlers

---
+++ assets/review.js
@@ -0,0 +1,20 @@
+alert('connected!');
+// apis you can use https://www.programmableweb.com/category/all/apis
+// incase you need to fill text https://doggoipsum.com/
+
+var li = document.querySelectorAll('li');
+// we select the li
+//add event listeners to all of the li's
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener('mouseover', function() {
+    this.classList.add('selected');
+  });
+  li[i].addEventListener('mouseout', function() {
+    this.classList.remove('selected');
+  });
+
+  li[i].addEventListener('click', function() {
+    this.classList.toggle('done');
+  });
+}
+
